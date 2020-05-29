67ceb0e5fdf719d9055d3acb06c587bfe677cfec
Justine Mignot <justinem@bam.tech>
2020-05-29 08:20:25 +0200

    :fire: (dist/) remove useless dist folder
    compile at yarn in projects

--- dist/automator/automator.cli.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-exports.__esModule = true;
-var automator_run_1 = require("./automator.run");
-automator_run_1.runInquirer();
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.runCollections = function () {
-    console.log("Automator package - not implemented yet");
-};
